1189dd42a1e8a08620c696b32d96eda3ef3293a0
PavelDushinskiy <89075143+PavelDushinskiy@users.noreply.github.com>
2022-07-26 16:34:07 +0300

    Added \\ in paths to os.path.join

--- clean_folder/clean.py
+++ clean_folder/clean.py
@@ -1,30 +1,17 @@
 """Procedure for sorting files in directory
-<<<<<<< HEAD
-=======
-
->>>>>>> d96a032 (Initial commit)
     All files and folders are renamed using the normalize function;
     File extensions do not change after renaming;
     Empty folders are deleted;
-    The unpacked contents of the archive are transferred to the archives folder
-    in a subfolder named the same as the archive;
+    The unpacked contents of the archive are transferred to the archives' folder
+    in a sub folder named the same as the archive;
     Files whose extensions are unknown remain unchanged.
-<<<<<<< HEAD
-"""
-
-import sys
-import os
-import re
-import shutil
-=======
-
 """
 
 import os
 import re
 import shutil
 import sys
->>>>>>> d96a032 (Initial commit)
+
 
 CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
@@ -38,7 +25,7 @@ for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
 
 def normalize(name: str) -> str:
     t_name = name.translate(TRANS)
-    t_name = re.sub(r'[^a-zA-Z0-9_\.]', '_', t_name)
+    t_name = re.sub(r'[^a-zA-Z\d_\.]', '_', t_name)
     return t_name
 
 
@@ -71,7 +58,7 @@ def tree_items(path):
     all_addresses = []
     for address, dirs, files in os.walk(path):
         for name in files:
-            all_addresses.append(os.path.join(address, name))
+            all_addresses.append(os.path.join(f'{address}\\{name}'))
     return all_addresses
 
 
@@ -86,13 +73,8 @@ def sort_files(folder_path):
     ext_list = list(extensions.items())
 
     for file_path in file_paths:
-<<<<<<< HEAD
-        extension = file_path.split('.')[-1]
-        file_name = normalize(file_path.split('\\')[-1])
-=======
         extension = str(file_path.split('.')[-1])
         file_name = normalize(str(file_path.split('\\')[-1]))
->>>>>>> d96a032 (Initial commit)
 
         for dict_key_int in range(len(ext_list)):
             if extension in ext_list[dict_key_int][1]:
@@ -127,18 +109,12 @@ def unpack_file(folder_path):
         os.remove(f'{folder_path}\\{archive}')
 
 
-<<<<<<< HEAD
 def main():
-=======
-if __name__ == "__main__":
->>>>>>> d96a032 (Initial commit)
     create_folders_from_list(root_folder, extensions)
     sort_files(root_folder)
     remove_empty_folders(root_folder)
     unpack_file(f'{root_folder}\\archives')
-<<<<<<< HEAD
+
 
 if __name__ == "__main__":
     main()
-=======
->>>>>>> d96a032 (Initial commit)
